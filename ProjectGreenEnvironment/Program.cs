deceaabe23086bdc2ede46f2920d92a1ab96a421
Rasmus Soeborg <ras@it-minds.dk>
2021-12-09 18:56:05 +0100

    initial

--- ProjectGreenEnvironment/Program.cs
+++ ProjectGreenEnvironment/Program.cs
@@ -30,9 +30,8 @@ namespace ProjectGreenEnvironment
             var environment = new GreenEnvironment();
             if (!Directory.Exists(GreenEnvironment.GetFolderPath()))
             {
-                FrmSetup setup = new FrmSetup();
-
-                string PeerName = default(string);
+                var setup = new FrmSetup();
+                var PeerName = default(string);
                 setup.NameRecieved += (s, e) => {
                     PeerName = (string)s;
                 };
@@ -40,17 +39,74 @@ namespace ProjectGreenEnvironment
                 setup.ShowDialog();
                 environment.Setup(PeerName);
             }
-
             environment.Load();
+            
+            // Connected devices
+            var connectedDevices = new List<BluetoothDeviceInfo>();
 
             // Create bluetooth handler
-            BluetoothHandler bluetooth = new BluetoothHandler("8724");
+            var bluetooth = new BluetoothHandler("8724");
+            Console.WriteLine($"Welcome back, mr. {environment.PeerName}");
+            
+            // Discover and connect to all "approved" devices"
+            bluetooth.DiscoverProgress += (s, e) => {
+                var devices = (BluetoothDeviceInfo[])s;
+                foreach (var device in devices)
+                {
+                    var btView = new BluetoothDeviceView(device);
 
-            // Open setup dialog
-            var connectDialog = new FrmConnect(bluetooth, environment);
-            Application.Run(connectDialog);
+                    if (!btView.Device.Remembered) continue;
+                    if (connectedDevices.Any(connectedDevice => connectedDevice.DeviceName == btView.Device.DeviceName)) continue;
+
+                    Console.WriteLine("Connecting to ... " + btView.Device.DeviceName);
+                    bluetooth.BeginConnect(btView.Device);
+                }
+            };
+
+            bluetooth.DiscoverComplete += (s, e) => {
+                // Permanent discovering...
+                bluetooth.BeginDiscoveringDevices();
+            };
+
+            bluetooth.ConnectedTo += (s, e) => {
+                var Response = (BluetoothConnectedData)((IAsyncResult)s).AsyncState;
+                var connectedToClient = new ConnectedBluetoothDeviceView(Response.Device)
+                    {Client = Response.SocketClient};
+                
+                connectedDevices.Add(connectedToClient.Device);
+                Console.WriteLine("Successfully connected to " + connectedToClient.Device.DeviceName);
+            };
+            
+
+            // Accept incoming connections
+            bluetooth.AcceptedConnection += (s, e) => {
+                var device = (BluetoothClient)s;
+                
+                // Start a new thread to listen to the device forever.
+                new Thread(() =>
+                {
+                    bluetooth.StartListeningTo(device);
+                }).Start();
+            };
+
+            // 
+            bluetooth.RecievedData += (s, e) =>
+            {
+                // On data receive
+                // Save data to file
+                var data = (BluetoothData)s;
+                environment.SaveFileData(data.Content);
+            };
+
+
+            // Start the bluetooth listening.
+            bluetooth.StartListener();
+            bluetooth.BeginDiscoveringDevices();
+            
+
+            while (true) {  Thread.Sleep(15); }
         }
 
-        
+
     }
 }
